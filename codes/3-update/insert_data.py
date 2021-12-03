ee52a003d5e15546bf5a06accb01c603b8bbd807
mohammad <mr.dori.dev@gmail.com>
2021-12-03 14:23:58 +0330

    update

---
+++ codes/3-update/insert_data.py
@@ -0,0 +1,41 @@
+"""Insert many values to database tables
+"""
+from mysql import connector
+
+database_connection = connector.connect(
+    host='localhost',
+    user='salar',
+    passwd='52212057',
+    database='university'
+)
+
+cursor = database_connection.cursor()
+
+# insert to faculty table
+SQL = """
+INSERT INTO faculty(
+    faculty_ID,
+    name,
+    admin,
+    fundation_year,
+    address
+)
+VALUES(%s, %s, %s, %s, %s)
+"""
+
+# insert value
+value = (1, 'computer', 'Dr Dori', '1390', 'tehran')
+cursor.execute(SQL, value)
+
+# insert many values
+values = [
+    (2, 'electronic', 'Dr Mohammady', '1390', 'shiraz'),
+    (3, 'mathematics', 'Dr Karimi', '1400', 'isfahan'),
+    (4, 'architecture', 'Mr Akbari', '1399', 'tabriz'),
+    (5, 'data science faculty', 'Dr Dori', '1390', 'mashhad')
+]
+cursor.executemany(SQL, values)
+
+
+# for change in database values
+database_connection.commit()
---
+++ codes/3-update/update_data.py
@@ -0,0 +1,23 @@
+"""update data from table
+"""
+
+from mysql import connector
+
+database_connection = connector.connect(
+    host='localhost',
+    user='salar',
+    passwd="52212057",
+    database='py_university'
+)
+
+cursor = database_connection.cursor()
+
+# update students table
+SQL = """
+UPDATE students
+SET entrance_year = entrance_year+1
+WHERE field_ID=1
+"""
+cursor.execute(SQL)
+
+database_connection.commit()
