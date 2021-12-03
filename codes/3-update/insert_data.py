8f5c223fce15b11d2871c75ca9e10e152de11511
mohammad <mr.dori.dev@gmail.com>
2021-12-03 15:27:20 +0330

    add any data

--- codes/3-update/insert_data.py
+++ codes/3-update/insert_data.py
@@ -27,16 +27,72 @@ VALUES(%s, %s, %s, %s, %s)
 """
 
 # insert value
-value = (1, 'computer', 'Dr Dori', '1390', 'tehran')
+value = (1, 'computer', 'Dr Dori', '1390', 'Tehran')
 cursor.execute(SQL, value)
 
 # insert many values
 values = [
-    (2, 'electronic', 'Dr Mohammady', '1390', 'shiraz'),
-    (3, 'mathematics', 'Dr Karimi', '1400', 'isfahan'),
-    (4, 'architecture', 'Mr Akbari', '1399', 'tabriz'),
-    (5, 'data science faculty', 'Dr Dori', '1390', 'mashhad')
+    (2, 'electronic', 'Dr Mohammadi', '1390', 'Isfahan'),
+    (3, 'mathematics', 'Dr Karimi', '1400', 'Shiraz'),
+    (4, 'architecture', 'Mr Akbari', '1399', 'Tehran'),
+    (6, 'data science', 'Mr Shahmoradi', '1390', 'Mashhad'),
+    (7, 'chemistry', 'Dr Sharifi', '1390', 'Tehran'),
 ]
+cursor.executemany(SQL, values)
+
+
+# insert to field table
+SQL = """
+INSERT INTO faculty(
+    field_ID,
+    field_name,
+    orientation,
+    grade,
+    faculty_ID
+)
+VALUES(%s, %s, %s, %s, %s)
+"""
+
+values = [
+    (1, 'computer', 'software', 'bachelor', 1),
+    (2, 'computer', 'hardware', 'bachelor', 1),
+    (3, 'computer', 'maching learning', 'master', 6),
+    (4, 'mathematics', 'pure mathematics', 'bachelor', 3),
+    (5, 'art', '3D', 'master', 4),
+    (6, 'electronic', 'robotic', 'master', 2),
+    (7, 'data science', 'image processing', 'bachelor', 6),
+    (8, 'computer', 'parallel processing', 'master', 1),
+    (9, 'chemistry', 'pure chemistry', 'bachelor', 7),
+]
+
+cursor.executemany(SQL, values)
+
+
+# insert to professors table
+SQL = """
+INSERT INTO faculty(
+    faculty_ID,
+    name,
+    admin,
+    fundation_year,
+    address
+)
+VALUES(%s, %s, %s, %s, %s)
+"""
+cursor.executemany(SQL, values)
+
+
+# insert to students table
+SQL = """
+INSERT INTO faculty(
+    faculty_ID,
+    name,
+    admin,
+    fundation_year,
+    address
+)
+VALUES(%s, %s, %s, %s, %s)
+"""
 cursor.executemany(SQL, values)
 
 
